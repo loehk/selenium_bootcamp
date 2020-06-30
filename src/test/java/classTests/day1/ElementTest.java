90472e1bcd9da0bdb32acb20cb84c90cff0e1ea6
loehk <evija.fokrote@gmail.com>
2020-06-30 18:15:22 +0300

    Evija Fokrote - Day1

--- src/test/java/classTests/day1/ElementTest.java
+++ src/test/java/classTests/day1/ElementTest.java
@@ -1,4 +1,4 @@
-package samplesPages;
+package classTests.day1;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -8,10 +8,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-public class Activity1 {
+public class ElementTest {
 
     WebDriver driver;
-    private String url = "http://automationpractice.com/index.php";
+    private String url = "https://kristinek.github.io/site/examples/actions";
 
     @BeforeClass
     public void setDriver() {
@@ -19,22 +19,20 @@ public class Activity1 {
         System.setProperty("webdriver.chrome.driver", userdirLib + "chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get(url);
     }
-
 
     @Test
     public void findElements() {
+        WebElement inputText = driver.findElement(By.id("text"));
+        inputText.clear();
+        inputText.sendKeys("Hello All!");
 
-        //find element "Search field"
-        WebElement searchField = driver.findElement(By.xpath("//input[@id='search_query_top']"));
+        WebElement resultButton = driver.findElement(By.id("result_button_text"));
+        resultButton.click();
 
-        //find element "magnifier glass"
-        WebElement magnifier = driver.findElement(By.xpath("//button[@name='submit_search']"));
-
-        // find element in menu "Woman"
-        WebElement menuWoman = driver.findElement(By.xpath("//a[@title='Women']"));
-
-
+        WebElement checkBox1 = driver.findElement(By.cssSelector("[id='vfb-6-0']"));
+        checkBox1.click();
     }
 
     @AfterClass
